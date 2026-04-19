877ecf3ade6f9af71b2679895cc036a52889a744
Marcin Skrzat <skrzatmarcin@gmail.com>
2026-04-19 10:27:07 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import LoginGate from './components/LoginGate';
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes, useRoutes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
