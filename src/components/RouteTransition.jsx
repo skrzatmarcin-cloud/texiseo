47ed7386db5c4c855e45e984c3fa7e77dd101808
Marcin Skrzat <skrzatmarcin@gmail.com>
2026-04-19 19:07:32 +0000

    File changes

---
+++ src/components/RouteTransition.jsx
@@ -0,0 +1,17 @@
+import { AnimatePresence, motion } from "framer-motion";
+
+export default function RouteTransition({ children }) {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
